6ee7b155c58a8c355e30df55f94956fda346e9b1
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-25 21:56:12 +0100

    refactor: Update dashboard layout component to remove commented code and add children container

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+// import { Footer, Sidebar, TopMenu } from "@/components";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="min-h-screen">
+      {/* <TopMenu />
+      <Sidebar /> */}
+      <div className="px-0 sm:px-0">{children}</div>
+      {/* <Footer /> */}
+    </main>
+  );
+}
