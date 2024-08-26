76cacaa1ec216bf01f2dc78855447f1c4fc63548
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-26 22:25:52 +0100

    update readme

--- src/app/(dashboard)/invoices/page.tsx
+++ src/app/(dashboard)/invoices/page.tsx
@@ -1,7 +1,10 @@
+import { InvoiceGrid } from "@/components";
+
 export default function InvoicePage() {
   return (
     <div>
-      <h1>Hello Page</h1>
+        {/*TODO: Provide a list of invoices for testing */}
+        <InvoiceGrid invoices={[]} />
     </div>
   );
 }
