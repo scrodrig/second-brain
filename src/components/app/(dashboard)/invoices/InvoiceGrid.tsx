54be5263b5fce4746dbc67c120d1c81cffb5a675
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-29 00:05:11 +0100

    feat: Refactor InvoiceGrid to use lazy loading for images

--- src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
+++ src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
@@ -1,10 +1,13 @@
 import { Invoice } from "@/interfaces";
 import { InvoiceGridItem } from "./InvoiceGridItem";
+import { getInvoices } from "@/actions/invoices/get-invoices";
 
 interface Props {
   invoices: Invoice[];
 }
 export const InvoiceGrid = ({ invoices }: Props) => {
+
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-10 mb-10">
       {invoices.map((invoice) => {
