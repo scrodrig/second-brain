e5beac80f281d7c84c55803a62c62b3baeedf343
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-29 02:19:49 +0100

    feat: Update InvoiceGridItem component to use NextUI Card component
    This commit updates the InvoiceGridItem component in the InvoiceGridItem.tsx file. It replaces the existing HTML structure with the NextUI Card component for a more modern and consistent design. This change improves the visual presentation of the invoice grid items, enhancing the overall user experience.

--- src/components/app/(dashboard)/invoices/InvoiceGridItem.tsx
+++ src/components/app/(dashboard)/invoices/InvoiceGridItem.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Chip,
+  Divider,
+  Link,
+} from "@nextui-org/react";
+
+import { CONCEPT_COLORS } from "@/utils";
 import { Invoice } from "@/interfaces";
-import Link from "next/link";
 
 interface Props {
   invoice: Invoice;
@@ -9,14 +19,42 @@ interface Props {
 
 export const InvoiceGridItem = ({ invoice }: Props) => {
   return (
-    <div className="rounded-md overflow-hidden fade-in">
-      <Link href={`/invoice/${invoice.id}`}>
-        <div className="bg-gray-200 h-40 w-full">{invoice.id}</div>
-      </Link>
-      <div className="p-4 flex flex-col">
-        <span className="font-bold">{invoice.concept}</span>
-        <span className="font-bold">${invoice.total}</span>
-      </div>
-    </div>
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        {/* <Image
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        /> */}
+        <div className="flex flex-col w-full">
+          <p className="text-md font-bold text-center">{invoice.place}</p>
+          <Chip size="sm" color="primary" variant="flat">NIF: {invoice.nif}</Chip>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <div className="p-4 flex flex-col">
+          <p className="">{invoice.description}</p>
+          <span className="font-bold">{invoice.total}</span>
+        </div>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <div>
+          <Chip
+            size="lg"
+            variant="shadow"
+            classNames={{
+              base: CONCEPT_COLORS[invoice.concept],
+              content: "drop-shadow shadow-black text-white",
+            }}
+          >
+            {invoice.concept}
+          </Chip>
+        </div>
+      </CardFooter>
+    </Card>
   );
 };
