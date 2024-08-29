f480d0accbdea545651610a0b1e9c7cfea5a0959
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-29 23:23:49 +0100

    refactor: Update InvoiceGridItem component to use NextUI Card component
    This commit updates the InvoiceGridItem component to replace the existing HTML structure with the NextUI Card component. The change improves the visual presentation of the invoice grid items, enhancing the overall user experience.

--- src/components/app/(dashboard)/invoices/InvoiceGridItem.tsx
+++ src/components/app/(dashboard)/invoices/InvoiceGridItem.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { CONCEPT_COLORS, currencyFormat } from "@/utils";
 import {
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -10,10 +10,15 @@ import {
   Divider,
   Link,
 } from "@nextui-org/react";
+import { CONCEPT_COLORS, currencyFormat } from "@/utils";
 import { IoCalendar, IoCalendarOutline } from "react-icons/io5";
 
+import { AiTwotoneBank } from "react-icons/ai";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
+import { GrLinkNext } from "react-icons/gr";
 import Image from "next/image";
 import { Invoice } from "@/interfaces";
+import { LiaPiggyBankSolid } from "react-icons/lia";
 
 interface Props {
   invoice: Invoice;
@@ -29,58 +34,84 @@ export const InvoiceGridItem = ({ invoice }: Props) => {
           src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
           width={40}
         /> */}
-        <div className="flex flex-col w-full">
-          <p className="text-3xl font-bold text-center mb-5 text-ellipsis">
-            {invoice.place}
-          </p>
 
-          <div className="flex sm:flex-row justify-between">
-            <Chip size="sm" color="primary" variant="flat">
+        <div className="flex flex-col w-full">
+          <span className="text-3xl font-bold text-center mb-5 overflow-hidden text-nowrap text-ellipsis">
+            {invoice.place}
+          </span>
+
+          <div className="flex flex-col w-full justify-center items-center">
+            <Divider className="mb-2" />
+            <Chip
+              size="md"
+              variant="shadow"
+              classNames={{
+                base: CONCEPT_COLORS[invoice.concept].className,
+                content: "drop-shadow shadow-black text-white",
+              }}
+            >
+              {invoice.concept}
+            </Chip>
+          </div>
+          <Divider className="mt-3 mb-4" />
+
+          <div className="flex sm:flex-row justify-around">
+            <Chip
+              size="md"
+              color="primary"
+              variant="flat"
+              classNames={{ content: "dark:text-white" }}
+            >
               NIF: {invoice.nif}
             </Chip>
 
-            <Chip size="sm" color="danger" variant="dot">
+            <Chip size="md" color="danger" variant="dot">
               {invoice.owner}
             </Chip>
           </div>
         </div>
       </CardHeader>
       <Divider />
-      <CardBody>
-        <div className="p-4 flex flex-col">
+      <CardBody className="flex items">
+        <div className="p-4 flex flex-col h-fit">
           <p className="font-light mt-0">Price:</p>
-
           <div className="text-5xl text-right">
-            <div className="font-medium text-green-800 hover:text-green-700 hover:font-bold transition-colors duration-150 ease-in-out">
+            <div className="font-medium text-green-800 dark:text-gray-50 hover:text-green-700 hover:font-bold transition-colors duration-150 ease-in-out">
               {currencyFormat(invoice.total)}
             </div>
           </div>
 
-          <p className="font-light mt-5 text-ellipsis">{invoice.description}</p>
+          <blockquote className="font-light mt-5 text-ellipsis">{`"${invoice.description}"`}</blockquote>
 
           <div className="mt-3 flex justify-start items-center">
-            <IoCalendarOutline className="mr-2" size={20} />
+            <IoCalendarOutline className="mr-2 text-sky-600" size={20} />
             {invoice.date.toDateString()}
           </div>
+
+          <div className="mt-3 flex justify-start items-center">
+            <AiTwotoneBank  className="mr-2 text-gray-600" size={20} />
+            {invoice.account}
+          </div>
+
+          <div className="mt-3 flex justify-start items-center">
+            <FaMoneyBillTransfer className="mr-2 text-green-600" size={20} />
+            {invoice.isReembursable ? "Reembursable" : "Not reembursable"}
+          </div>
+
+          {invoice.isReembursable && (
+            <div className="mt-3 flex justify-start items-center">
+              <LiaPiggyBankSolid className="mr-2 text-pink-600" size={20} />
+              {invoice.isRefunded ? "Refounded" : "Not Refounded"}
+            </div>
+          )}
         </div>
       </CardBody>
       <Divider />
       <CardFooter>
         <div className="flex w-full justify-end">
-          <Chip
-            size="lg"
-            variant="shadow"
-            classNames={{
-              base: CONCEPT_COLORS[invoice.concept].className,
-              content: "drop-shadow shadow-black text-white",
-            }}
-          >
-            {invoice.concept}
-          </Chip>
-          {/* <div className="mt-3 flex justify-end items-center">
-            <IoCalendarOutline className="mr-2" size={20} />
-            {invoice.date.toDateString()}
-          </div> */}
+          <Button color="primary" variant="solid" endContent={<GrLinkNext />}>
+            See more
+          </Button>
         </div>
       </CardFooter>
     </Card>
