e3006b207881a272b1302310461954efc9258f53
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-08-30 02:21:39 +0100

    feat: Add Navbar component with responsive layout and functionality

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,69 @@
+import { FcFeedback, FcSupport } from "react-icons/fc";
+import { Input, Link, Navbar, NavbarContent } from "@nextui-org/react";
+
+import { BurguerButton } from "./BurgerButton";
+import { IoSearch } from "react-icons/io5";
+import { NotificationsDropdown } from "./NotificationsDropdown";
+import React from "react";
+import { SlSocialGithub } from "react-icons/sl";
+import { UserDropdown } from "./UserDropdown";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const NavbarWrapper = ({ children }: Props) => {
+  return (
+    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+      <Navbar
+        isBordered
+        className="w-full"
+        classNames={{
+          wrapper: "w-full max-w-full",
+        }}
+      >
+        <NavbarContent className="md:hidden">
+          <BurguerButton />
+        </NavbarContent>
+        <NavbarContent className="w-full max-md:hidden">
+          <Input
+            startContent={<IoSearch />}
+            isClearable
+            className="w-full"
+            classNames={{
+              input: "w-full",
+              mainWrapper: "w-full",
+            }}
+            placeholder="Search..."
+          />
+        </NavbarContent>
+        <NavbarContent
+          justify="end"
+          className="w-fit data-[justify=end]:flex-grow-0"
+        >
+          <div className="flex items-center gap-2 max-md:hidden">
+            <FcFeedback />
+            <span>Feedback?</span>
+          </div>
+
+          <NotificationsDropdown />
+          
+          <div className="max-md:hidden">
+            <FcSupport />
+          </div>
+
+          <Link
+            href="https://github.com/scrodrig"
+            target={"_blank"}
+          >
+            <SlSocialGithub />
+          </Link>
+          <NavbarContent>
+            <UserDropdown />
+          </NavbarContent>
+        </NavbarContent>
+      </Navbar>
+      {children}
+    </div>
+  );
+};
