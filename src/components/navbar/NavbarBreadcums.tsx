5fbbc15869f179f27995ec0944055f5f4b2ab17b
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-09-01 22:57:29 +0100

    feat: Add NavbarBreadcums component to display breadcrumb navigation
    This commit adds the NavbarBreadcums component to the project. The component is responsible for rendering the breadcrumb navigation in the navbar. It uses the NextUI Breadcrumbs and BreadcrumbItem components to display the breadcrumb items based on the current router path. The getBreadcums function from the utils module is used to generate the breadcrumb data. This addition enhances the navigation experience by providing a clear path for users to navigate back to previous pages.

---
+++ src/components/navbar/NavbarBreadcums.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+
+import { getBreadcums } from "@/utils";
+import { usePathname } from "next/navigation";
+
+export const NavbarBreadcums = () => {
+  const router = usePathname();
+
+  const breadcums = getBreadcums(router);
+  console.log("🚀 ~ NavbarBreadcums ~ breadcums:", breadcums)
+
+  return (
+    <Breadcrumbs>
+      {
+        breadcums.map((item, index) => (
+          <BreadcrumbItem
+            key={index}
+            startContent={item.icon}
+          >
+            {item.title}
+          </BreadcrumbItem>
+        ))
+      }
+    </Breadcrumbs>
+  );
+};
+
+
