05e26a8350bffeb4e254652606d139bff2e58380
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 00:08:07 +0100

    feat: Add getInvoiceById function to actions
    This commit adds the getInvoiceById function to the actions/index.ts file. This function is responsible for fetching invoice details by ID.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
+import { getInvoiceById } from "./invoices/get-invoice-by-id";
 import { getInvoices } from "./invoices/get-invoices";
 
-export { getInvoices };
+export { getInvoices, getInvoiceById };
