8b04dae16509fdc45ac81c658c73ab6b59a47dc3
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 03:48:19 +0100

    Refactor InvoicePage component to remove unnecessary code and improve readability

--- src/app/(dashboard)/invoice/[id]/page.tsx
+++ src/app/(dashboard)/invoice/[id]/page.tsx
@@ -20,23 +20,7 @@ export default async function InvoicePage({ params: { id } }: Props) {
 
   return (
     <div>
-      <Card className="rounded-xl shadow-md text-center w-full">
-        <CardBody>
-          <div className="flex items-center gap-3">
-            <Image
-              alt="nextui logo"
-              height={32}
-              width={32}
-              src={`https://ui-avatars.com/api/?name=${invoice.place}&rounded=true&bold=true&background=FF6666`}
-              className="mx-2"
-            />
-            <h1>{invoice.place}</h1>
-          </div>
-        </CardBody>
-      </Card>
-
       <InvoiceDetails invoice={invoice} />
-      
     </div>
   );
 }
