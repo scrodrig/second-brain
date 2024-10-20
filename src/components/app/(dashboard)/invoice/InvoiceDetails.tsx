2aa9e3783ee84ebba85807cdd5a4393a87f4f906
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 19:18:32 +0100

    Refactor InvoiceDetails component to improve layout and display of account information, total amount, and invoice details

--- src/components/app/(dashboard)/invoice/InvoiceDetails.tsx
+++ src/components/app/(dashboard)/invoice/InvoiceDetails.tsx
@@ -1,10 +1,17 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Chip, Input } from "@nextui-org/react";
+import { FaArrowDown, FaX } from "react-icons/fa6";
+import {
+  FaCheckCircle,
+  FaCross,
+  FaCrosshairs,
+  FaMoneyBill,
+} from "react-icons/fa";
 
 import { AiTwotoneBank } from "react-icons/ai";
-import { FaArrowDown } from "react-icons/fa6";
-import { FaMoneyBill } from "react-icons/fa";
 import Image from "next/image";
 import { Invoice } from "@/interfaces";
+import { IoFastFoodOutline } from "react-icons/io5";
+import clsx from "clsx";
 import { currencyFormat } from "@/utils";
 import { formatWithOptions } from "util";
 
@@ -12,6 +19,14 @@ interface Props {
   invoice: Invoice;
 }
 export const InvoiceDetails = ({ invoice }: Props) => {
+  const checkIcon = (flag: boolean) => {
+    return flag ? (
+      <FaCheckCircle className="text-green-300" size={20} />
+    ) : (
+      <FaX className="text-red-200" size={20} />
+    );
+  };
+
   return (
     <>
       <div className="gap-5 text-5xl font-bold text-center">
@@ -33,16 +48,16 @@ export const InvoiceDetails = ({ invoice }: Props) => {
         </CardBody>
       </Card>
 
-      <div className="mt-10 mb-10 gap-5 grid grid-cols-1 sm:grid-cols-4">
+      <div className="mt-10 mb-10 gap-5 grid grid-cols-1 md:grid-cols-4">
         <Card className="group">
-            <Image
-              alt="Card background"
-              className=" w-full rounded-xl opacity-30 transform group-hover:scale-105 group-hover:opacity-70 transition-transform duration-300"
-              src="/imgs/account.png"
-              width={100}
-              height={250}
-            />
-          <CardBody className="flex flex-col sm:flex-row justify-around items-center">
+          <Image
+            alt="Card background"
+            className=" w-full rounded-xl opacity-30 transform group-hover:scale-105 group-hover:opacity-70 transition-transform duration-300"
+            src="/imgs/account.png"
+            width={100}
+            height={250}
+          />
+          <CardBody className="flex flex-col md:flex-row justify-around items-center">
             <div className="flex-col">
               <p className="text-tiny uppercase font-bold">Account</p>
               <h4 className="font-bold text-large">{invoice.account}</h4>
@@ -63,6 +78,78 @@ export const InvoiceDetails = ({ invoice }: Props) => {
                 <span className="text-red-600 mt-5">
                   {currencyFormat(invoice.total)}
                 </span>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+
+        <Card className="group col-span-1 md:col-span-2">
+          <CardHeader className="text-4xl font-bold underline text-center">
+            Invoice Details
+          </CardHeader>
+
+          <CardBody className="flex flex-col justify-start items-start overflow-hidden">
+            <div className="flex-col w-full">
+              <Input
+                type="date"
+                label="Date"
+                value={invoice.date.toISOString().split("T")[0]}
+                readOnly
+              />
+              <Input
+                className="mt-5"
+                type="string"
+                label="NIF"
+                value={invoice.nif}
+                readOnly
+              />
+
+              <Input
+                className="mt-5"
+                type="string"
+                startContent={<IoFastFoodOutline size={20} />}
+                color="warning"
+                label="Concept"
+                value={invoice.concept}
+                readOnly
+              />
+
+              <div className="grid grid-cols-1 xl:grid-cols-3 mt-10 xl:mt-20 place-items-center gap-5">
+                <Chip
+                  startContent={checkIcon(invoice.isReembursable)}
+                  variant="shadow"
+                  size="lg"
+                  className={clsx("text-white", {
+                    "bg-green-500": invoice.isReembursable,
+                    "bg-red-500": !invoice.isReembursable,
+                  })}
+                >
+                  {invoice.isReembursable ? "Reembursable" : "Not Reembursable"}
+                </Chip>
+
+                <Chip
+                  startContent={checkIcon(invoice.vatRefund ?? false)}
+                  variant="shadow"
+                  size="lg"
+                  className={clsx("text-white", {
+                    "bg-green-500": invoice.vatRefund,
+                    "bg-red-500": !invoice.vatRefund,
+                  })}
+                >
+                  {invoice.vatRefund ? "Applicable" : "Not applicable"}
+                </Chip>
+
+                <Chip
+                  startContent={checkIcon(invoice.isRefunded)}
+                  variant="shadow"
+                  size="lg"
+                  className={clsx("text-white", {
+                    "bg-green-500": invoice.isRefunded,
+                    "bg-red-500": !invoice.isRefunded,
+                  })}
+                >
+                  {invoice.vatRefund ? "Refounded" : "Not Refounded"}
+                </Chip>
               </div>
             </div>
           </CardBody>
