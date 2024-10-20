a55b1541a7f5a8950ed631e478edb62c399796a5
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 04:06:20 +0100

    Refactor InvoiceDetails component to improve layout and display of account information and total amount

--- src/components/app/(dashboard)/invoice/InvoiceDetails.tsx
+++ src/components/app/(dashboard)/invoice/InvoiceDetails.tsx
@@ -33,44 +33,40 @@ export const InvoiceDetails = ({ invoice }: Props) => {
         </CardBody>
       </Card>
 
-      <div className="mt-10 flex flex-col justify-center">
-        <div className="mt-10 w-full flex gap-10 sm:flex-nowrap flex-wrap">
-          <Card className="group">
+      <div className="mt-10 mb-10 gap-5 grid grid-cols-1 sm:grid-cols-4">
+        <Card className="group">
             <Image
               alt="Card background"
-              className="object-cover rounded-xl opacity-30 transform group-hover:scale-105 group-hover:opacity-70 transition-transform duration-300"
+              className=" w-full rounded-xl opacity-30 transform group-hover:scale-105 group-hover:opacity-70 transition-transform duration-300"
               src="/imgs/account.png"
-              width={250}
+              width={100}
               height={250}
             />
-            <CardBody className="flex flex-row justify-around items-center">
-              <div className="flex-col">
-                <p className="text-tiny uppercase font-bold">Account</p>
-                <h4 className="font-bold text-large">{invoice.account}</h4>
-              </div>
-              <AiTwotoneBank className="mr-2 text-gray-300" size={80} />
-            </CardBody>
-          </Card>
+          <CardBody className="flex flex-col sm:flex-row justify-around items-center">
+            <div className="flex-col">
+              <p className="text-tiny uppercase font-bold">Account</p>
+              <h4 className="font-bold text-large">{invoice.account}</h4>
+            </div>
+            <AiTwotoneBank className="mr-2 text-gray-300" size={80} />
+          </CardBody>
+        </Card>
 
-          <Card className="group">
-            <CardBody className="flex flex-row justify-center items-center overflow-hidden">
-              <div className="flex-col justify-center text-center">
-                <div className="flex justify-center items-center transform group-hover:scale-110 group-hover:rotate-3 transition-transform duration-300">
-                  <FaMoneyBill className="mr-2 text-green-800" size={100} />
-                </div>
-                <p className="text-tiny uppercase font-bold text-center">
-                  Total
-                </p>
-                <div className="flex flex-row font-bold text-7xl items-center">
-                  <span className="text-red-600">
-                    {currencyFormat(invoice.total)}
-                  </span>
-                  <FaArrowDown className="ml-2 text-red-600" size={50} />
-                </div>
+        <Card className="group">
+          <CardBody className="flex flex-row justify-center items-center overflow-hidden">
+            <div className="flex-col justify-center text-center">
+              <div className="flex justify-center items-center transform group-hover:scale-110 group-hover:rotate-3 transition-transform duration-300">
+                <FaMoneyBill className="mr-2 text-green-800" size={100} />
               </div>
-            </CardBody>
-          </Card>
-        </div>
+              <p className="text-tiny uppercase font-bold text-center">Total</p>
+              <div className="flex flex-col font-bold text-2xl md:text-5xl lg:text-6xl items-center">
+                <FaArrowDown className="mt-5 text-red-600" size={50} />
+                <span className="text-red-600 mt-5">
+                  {currencyFormat(invoice.total)}
+                </span>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
       </div>
     </>
   );
