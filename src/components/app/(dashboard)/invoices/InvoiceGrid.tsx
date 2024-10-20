76bc9e5e202363839836bb5c1b00a17a9eb7212d
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 03:48:13 +0100

    Refactor InvoiceGrid to display 3 columns instead of 4

--- src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
+++ src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
@@ -8,7 +8,7 @@ export const InvoiceGrid = ({ invoices }: Props) => {
 
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mb-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 mb-10">
       {invoices.map((invoice) => {
         return <InvoiceGridItem key={invoice.id} invoice={invoice} />;
       })}
