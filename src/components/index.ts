a61ea32bb02faa389b1519681ea057354c715ce7
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-10-20 02:59:49 +0100

    Refactor index.ts to add InvoiceForm and InvoiceDetails components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { InvoiceGrid } from "./app/(dashboard)/invoices/InvoiceGrid";
 export { SidebarContainer } from "./sidebar/Sidebar";
 export { NavbarWrapper } from "./navbar/Navbar";
+export { InvoiceForm } from "./app/(dashboard)/invoice/InvoiceForm";
+export { InvoiceDetails } from "./app/(dashboard)/invoice/InvoiceDetails";
