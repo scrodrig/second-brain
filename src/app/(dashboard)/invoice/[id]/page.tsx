6ddf4e0c2db330bebd30442bc55712ceb2cb4b77
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-04 22:58:08 +0000

    refactor: Destructure invoice from getInvoiceById response in InvoicePage component

--- src/app/(dashboard)/invoice/[id]/page.tsx
+++ src/app/(dashboard)/invoice/[id]/page.tsx
@@ -1,6 +1,3 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/react";
-
-import Image from "next/image";
 import { InvoiceDetails } from "@/components";
 import { getInvoiceById } from "@/actions";
 import { notFound } from "next/navigation";
@@ -12,7 +9,7 @@ interface Props {
 }
 
 export default async function InvoicePage({ params: { id } }: Props) {
-  const invoice = await getInvoiceById(id);
+  const { invoice } = await getInvoiceById(id);
 
   if (!invoice) {
     notFound();
