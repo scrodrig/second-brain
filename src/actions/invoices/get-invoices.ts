1d5bfce0f38eadf61a5c9ddf6323d409c19ceba5
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-04 23:20:11 +0000

    feat: Implement pagination for invoice retrieval in getInvoices function

--- src/actions/invoices/get-invoices.ts
+++ src/actions/invoices/get-invoices.ts
@@ -1,17 +1,37 @@
-'use server'
+"use server";
 
-import prisma from '@/lib/prisma'
+import prisma from "@/lib/prisma";
 
-export const getInvoices = async () => {
-
-  const invoices = await prisma.invoice.findMany({
-    orderBy: {
-      date: 'desc',
-    },
-  })
-
-  return {
-    success: true,
-    invoices: invoices,
-  }
+interface PaginationOptions {
+  page?: number;
+  take?: number;
 }
+
+export const getInvoices = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  try {
+    if (isNaN(Number(page))) page = 1;
+    if (page < 1) page = 1;
+
+    const invoices = await prisma.invoice.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    const totalCount = await prisma.invoice.count({});
+    const totalPages = Math.ceil(totalCount / take);
+
+    return {
+      currentPage: page,
+      totalPages,
+      invoices: invoices,
+    };
+  } catch (e) {
+    throw new Error("Products not loaded");
+  }
+};
