0e9d96a8460269a2e6fa0b01ed5155679858b29a
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-04 23:20:31 +0000

    feat: Export generatePaginationNumbers utility from index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
-export * from './ui/invoice/concept-colors';
-export * from './formater/currency-format';
-export * from './ui/breadcums/icons-menu';
+export * from "./ui/invoice/concept-colors";
+export * from "./formater/currency-format";
+export * from "./ui/breadcums/icons-menu";
+export * from "./ui/pagination/generatePaginationNumbers";
