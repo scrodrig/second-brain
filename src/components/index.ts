3ef60239406328cae6c18a49674ae4d072983cd0
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 20:33:03 +0000

    feat: Import BlockRadio component in index for centralized exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { Pagination } from './ui/pagination/Pagination';
+import { BlockRadio } from "./ui/radio/BlockRadio";
+export { Pagination } from "./ui/pagination/Pagination";
 export { InvoiceGrid } from "./app/(dashboard)/invoices/InvoiceGrid";
 export { SidebarContainer } from "./sidebar/Sidebar";
 export { NavbarWrapper } from "./navbar/Navbar";
@@ -6,3 +7,4 @@ export { InvoiceForm } from "./app/(dashboard)/invoice/InvoiceForm";
 export { InvoiceDetails } from "./app/(dashboard)/invoice/InvoiceDetails";
 export { Footer } from "./ui/footer/Footer";
 export { Title } from "./ui/title/Title";
+export { BlockRadio } from "./ui/radio/BlockRadio";
