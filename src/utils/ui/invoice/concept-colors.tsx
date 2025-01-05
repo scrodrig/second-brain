6201d624f6079f746370674775b2f4f495e513d9
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 22:22:12 +0000

    feat: Enhance concept colors utility to include icons for better visual representation

--- src/utils/ui/invoice/concept-colors.tsx
+++ src/utils/ui/invoice/concept-colors.tsx
@@ -1,7 +1,11 @@
+import { FaBowlFood, FaBus, FaChrome, FaComputer } from "react-icons/fa6";
 import React, { ReactNode } from "react";
 
 import { Concept } from "@/interfaces";
 import { IoCalendar } from "react-icons/io5";
+import { IoMdSchool } from "react-icons/io";
+import { MdElectricBolt } from "react-icons/md";
+import { VscSymbolMisc } from "react-icons/vsc";
 
 export const CONCEPT_COLORS_GRADIENT: Record<Concept, string> = {
   Meal: "bg-gradient-to-br from-indigo-500 to-pink-500 border-small border-white/50 shadow-pink-500/30",
@@ -23,11 +27,32 @@ export const CONCEPT_COLORS: Record<
   Concept,
   { className: string; icon: ReactNode }
 > = {
-  Meal: { className: "bg-green-300", icon: <IoCalendar /> },
-  Transportation: { className: "bg-red-300", icon: <IoCalendar /> },
-  Technology: { className: "bg-amber-300", icon: <IoCalendar /> },
-  Internet: { className: "bg-violet-300", icon: <IoCalendar /> },
-  Electricity: { className: "bg-blue-300", icon: <IoCalendar /> },
-  Education: { className: "bg-orange-300", icon: <IoCalendar /> },
-  Others: { className: "bg-gray-500", icon: <IoCalendar /> },
+  Meal: {
+    className: "bg-green-300",
+    icon: <FaBowlFood className="mr-2" size={20} />,
+  },
+  Transportation: {
+    className: "bg-red-300",
+    icon: <FaBus className="mr-2" size={20} />,
+  },
+  Technology: {
+    className: "bg-amber-300",
+    icon: <FaComputer className="mr-2" size={20} />,
+  },
+  Internet: {
+    className: "bg-violet-300",
+    icon: <FaChrome className="mr-2" size={20} />,
+  },
+  Electricity: {
+    className: "bg-blue-300",
+    icon: <MdElectricBolt className="mr-2" size={20} />,
+  },
+  Education: {
+    className: "bg-orange-300",
+    icon: <IoMdSchool className="mr-2" size={20} />,
+  },
+  Others: {
+    className: "bg-gray-500",
+    icon: <VscSymbolMisc className="mr-2" size={20} />,
+  },
 };
