65c3197873ff3bcf94585d29a21340e51fc55438
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 01:20:52 +0000

    feat: Enhance Home component with image and welcome message

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello second brain
+    <div className="flex flex-col justify-center items-center">
+      <Image
+        src="/imgs/home.png"
+        width={750}
+        height={750}
+        alt="Picture of the author"
+        className="rounded-full aspect-square object-cover transition-transform duration-500 hover:rotate-3"
+      />
+      <h1 className="text-5xl font-bold my-5">SECOND BRAIN</h1>
+      <p className="text-black-700 dark:text-gray-700 text-center">
+        Welcome to your second brain. A place to store all your thoughts and
+        financial data.
+      </p>
     </div>
   );
 }
