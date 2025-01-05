9f3437f0aa54e6bfc61a7569b43e900ba289b77d
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 01:20:55 +0000

    feat: Export Footer component from index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { SidebarContainer } from "./sidebar/Sidebar";
 export { NavbarWrapper } from "./navbar/Navbar";
 export { InvoiceForm } from "./app/(dashboard)/invoice/InvoiceForm";
 export { InvoiceDetails } from "./app/(dashboard)/invoice/InvoiceDetails";
+export { Footer } from "./ui/footer/Footer";
