abfbb6c8f1844bdc4f89036e21d4c48ce915323f
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 20:32:32 +0000

    feat: Export getAccounts and getConcepts functions from index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,6 @@
+import { getAccounts } from "./invoices/accounts/get-accounts";
+import { getConcepts } from "./invoices/concepts/get-concepts";
 import { getInvoiceById } from "./invoices/get-invoice-by-id";
 import { getInvoices } from "./invoices/get-invoices";
 
-export { getInvoices, getInvoiceById };
+export { getInvoices, getInvoiceById, getConcepts, getAccounts };
