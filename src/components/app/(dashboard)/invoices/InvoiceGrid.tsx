1ec5d363c43cf876150d0f6f2902804df021939c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2025-01-05 03:08:20 +0000

    feat: Replace "New Invoice" button with a link to the new invoice page in InvoiceGrid

--- src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
+++ src/components/app/(dashboard)/invoices/InvoiceGrid.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/react";
 import { Invoice } from "@/interfaces";
 import { InvoiceGridItem } from "./InvoiceGridItem";
 import { IoMdAdd } from "react-icons/io";
+import Link from "next/link";
 
 interface Props {
   invoices: Invoice[];
@@ -9,10 +10,10 @@ interface Props {
 export const InvoiceGrid = ({ invoices }: Props) => {
   return (
     <>
-      <div className="flex justify-end">
-      <Button className="mb-5" color="primary" endContent={<IoMdAdd />}>
-        New Invoice
-      </Button>
+      <div className="flex justify-end mb-5">
+        <Link href="/invoices/new" className="btn-primary">
+          <IoMdAdd />
+        </Link>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mb-10">
         {invoices.map((invoice) => {
