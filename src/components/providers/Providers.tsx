9fb7499f70c40f2784e0d0cd443d60d759e0c4c3
Schubert D Rodriguez <schubert.david.r@gmail.com>
2026-03-30 14:38:21 +0100

    fix: add SessionProvider to enable useSession in client components

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import type { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
+    <SessionProvider>
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </NextThemesProvider>
+    </SessionProvider>
   );
 }
